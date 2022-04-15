02bf5cc9d0cdd859cbc7c19656bdc626d9cd25ef
jbonaiuto <jbonaiuto@gmail.com>
2022-04-15 16:50:17 +0200

    updated to work with new calibration folder structure

--- run_sba.py
+++ run_sba.py
@@ -1,71 +1,75 @@
 import json
 import pickle
 import sys
+import os
 from datetime import datetime
 
 import numpy as np
 
 from utilities.calib_tools import bundle_adjust_points_and_extrinsics
 
-intrinsic_file = sys.argv[1]
-extrinsic_file = sys.argv[2]
-sba_file = sys.argv[3]
+def run_sba(params, calib_folder):
+    handle = open(os.path.join(calib_folder, "sba_data.pickle"), 'rb')
+    sba_data = pickle.load(handle)
+    handle.close()
 
-try:
-    json_file = sys.argv[4]
-    print("USING: ", json_file)
-except:
-    json_file = "settings.json"
-    print("USING: ", json_file)
+    handle = open(os.path.join(calib_folder, "intrinsic_params.pickle"), 'rb')
+    intrinsic_params = pickle.load(handle)
+    handle.close()
 
-# opening a json file
-with open(json_file) as settings_file:
-    params = json.load(settings_file)
+    handle = open(os.path.join(calib_folder, "extrinsic_params.pickle"), 'rb')
+    extrinsic_params = pickle.load(handle)
+    handle.close()
 
-# Open intrinsic parameters
-handle = open(intrinsic_file, "rb")
-intrinsic_params = pickle.load(handle)
-handle.close()
+    # Convert parameters from dictionaries to arrays
+    k_arr = np.array([intrinsic_params[x]['k'] for x in params['cam_sns']])
+    d_arr = np.array([intrinsic_params[x]['d'] for x in params['cam_sns']])
+    r_arr = np.array([extrinsic_params[x]['r'] for x in params['cam_sns']])
+    t_arr = np.array([extrinsic_params[x]['t'] for x in params['cam_sns']])
 
-# Open extrinsic parameters
-handle = open(extrinsic_file, "rb")
-extrinsic_params = pickle.load(handle)
-handle.close()
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
 
-# Open SBA data
-handle = open('sba_data.pickle', "rb")
-sba_data = pickle.load(handle)
-handle.close()
+    # Run SBA
+    obj_pts, r_arr, t_arr, res = bundle_adjust_points_and_extrinsics(np.squeeze(sba_data['points_2d']),
+                                                                     np.squeeze(sba_data['points_3d']),
+                                                                     sba_data['points_3d_indices'],
+                                                                     sba_data['camera_indices'], k_arr, d_arr, r_arr, t_arr)
+    print(f"\nBefore: mean: {np.mean(res['before'])}, std: {np.std(res['before'])}")
+    print(f"After: mean: {np.mean(res['after'])}, std: {np.std(res['after'])}\n")
 
-# Convert parameters from dictionaries to arrays
-k_arr = np.array([intrinsic_params[x]['k'] for x in params['cam_sns']])
-d_arr = np.array([intrinsic_params[x]['d'] for x in params['cam_sns']])
-r_arr = np.array([extrinsic_params[x]['r'] for x in params['cam_sns']])
-t_arr = np.array([extrinsic_params[x]['t'] for x in params['cam_sns']])
+    # Save new extrinsic parameters
+    extrinsic_sba_params = {}
+    for cam_idx, sn in enumerate(params['cam_sns']):
+        extrinsic_sba_params[sn] = {
+            'r': np.squeeze(r_arr[cam_idx, :, :]),
+            't': t_arr[cam_idx, :, :]
+        }
 
-timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    filename = os.path.join(calib_folder, "extrinsic_sba_params.pickle")
+    pickle.dump(
+        extrinsic_sba_params,
+        open(
+            filename,
+            "wb",
+        ),
+    )
 
-# Run SBA
-obj_pts, r_arr, t_arr, res = bundle_adjust_points_and_extrinsics(np.squeeze(sba_data['points_2d']),
-                                                                 np.squeeze(sba_data['points_3d']),
-                                                                 sba_data['points_3d_indices'],
-                                                                 sba_data['camera_indices'], k_arr, d_arr, r_arr, t_arr)
-print(f"\nBefore: mean: {np.mean(res['before'])}, std: {np.std(res['before'])}")
-print(f"After: mean: {np.mean(res['after'])}, std: {np.std(res['after'])}\n")
+if __name__=='__main__':
+    try:
+        json_file = sys.argv[1]
+        print("USING: ", json_file)
+    except:
+        json_file = "settings.json"
+        print("USING: ", json_file)
 
-# Save new extrinsic parameters
-extrinsic_sba_params = {}
-for cam_idx, sn in enumerate(params['cam_sns']):
-    extrinsic_sba_params[sn] = {
-        'r': np.squeeze(r_arr[cam_idx, :, :]),
-        't': t_arr[cam_idx, :, :]
-    }
+    try:
+        calib_folder = sys.argv[2]
+        print('USING: %s' % calib_folder)
+    except:
+        calib_folder = None
 
-filename = "extrinsic_sba_params_{}.pickle".format(timestamp)
-pickle.dump(
-    extrinsic_sba_params,
-    open(
-        filename,
-        "wb",
-    ),
-)
+    # opening a json file
+    with open(json_file) as settings_file:
+        params = json.load(settings_file)
+
+    run_sba(params, calib_folder)
